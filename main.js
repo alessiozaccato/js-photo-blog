77fed8f12f3e31d26dbe1c023b75091b9754577d
alessiozaccato <alessiozaccato@gmail.com>
2025-01-29 15:32:56 +0100

    ADD:milestone 2

--- main.js
+++ main.js
@@ -1,2 +1,36 @@
 //let's link from html
-let title = document.getElementById("htmlTitle")
+let card = document.getElementById("htmlCard");
+
+
+
+fetch("https://lanciweb.github.io/demo/api/pictures/")
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(element => {
+            card.innerHTML +=
+                `
+            <div class="col-md-6 col-lg-4">
+                <div class="card mt-4">
+                    <figure class="position-relative">
+                        <img class="img-fluid p-3" src="${element.url}" alt="${element.title}">
+                    </figure>
+                    <a href="#">
+                        <img class="position-absolute top-0 start-50 translate-middle-x" src="./img/pin.svg" alt="pin">
+                    </a>
+                    <div class="card-body">
+                        <p class="sometype-mono-regular fs-4 text-danger">${element.date} </p>
+                        <p class="edu-tas-beginner-paragraph">${element.title} </p>
+                    </div>
+            </div>
+               
+            `
+        });
+    })
+    .catch(error => {
+
+        console.error(error);
+
+    });
+
+
+
